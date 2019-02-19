cc00d1a30e5f2c5fb790b9c8d4003ee8d9d90a08
Alessandro De Cenzo <decenzo.a@gmail.com>
2019-02-19 12:59:39 +0100

    First Commit

---
+++ app/pages/audit.vue
@@ -0,0 +1,22 @@
+<template>
+    <f7-page pull-to-refresh @ptr:refresh="onRefresh">
+        <div data-page="PAGENAME">
+            <f7-navbar :title="$root.config.title" />
+            <f7-block>
+                <div class="page-content ">
+                    <div id="PAGEPlaceHolder"></div>
+                </div>
+            </f7-block>
+        </div>
+    </f7-page>
+</template>
+<!--
+<script>
+    myApp.onPageInit('PAGENAME', function (page) {
+
+        $$.get('https://tools.blankwebagency.com/index.php/audit', {}, function (data) {
+            $$('#PAGEPlaceHolder').html(data);
+        });
+    });
+</script>
+-->
---
+++ app/pages/home.vue
@@ -0,0 +1,22 @@
+<template>
+  <f7-page pull-to-refresh @ptr:refresh="onRefresh">
+    <f7-navbar :title="$root.config.title" />
+    <f7-block inner inset>{{$lang('text')}}</f7-block>
+    <f7-block inner inset>Funzia!</f7-block>
+    <f7-block inner inset>
+        <a class="link" href="https://tools.blankwebagency.com/index.php/audit">
+            <i class="icon"></i>
+            <span>Fai un Audit</span>
+        </a>
+    </f7-block>
+        <f7-block inner inset>
+            <a class="link" href="/audit/">
+                <f7-icon material="open_in_browser" size="18" style="padding-bottom: 3px" />
+                <span>Fai un Audit</span>
+            </a>
+        </f7-block>
+    <f7-block>
+      <f7-button big fill raised bg="green" href="https://github.com/scriptPilot/app-framework/blob/master/DOCUMENTATION.md" external>Documentation</f7-button>
+    </f7-block>
+  </f7-page>
+</template>
